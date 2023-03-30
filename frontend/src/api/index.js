7e728f1ddadb917c7ed4601d79056b2996173948
Roshan Deshmukh <roshan.rdx2@gmail.com>
2023-03-30 10:56:51 -0500

    removed key

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,25 +1,5 @@
 import axios from 'axios';
 
-// export const getWeatherData = async (lat,lng)=> {
-
-//     try{
-//         const {data} = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?`,{
-//         params:{
-//             lat:lat,
-//             lng:lng
-//         },  
-//         headers: {
-//                 'X-RapidAPI-Key': 'ded30534e9c0ac67f44b45dc36aa10a9',
-//               }
-//         });
-
-//         return data;
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
-
-
 export const getCountyData = async (lat,lng)=>{
     try{
         
